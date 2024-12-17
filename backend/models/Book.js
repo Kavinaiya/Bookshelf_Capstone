39e6ac4ca254a43728e8ef8ddcc94508ba800fb0
Kavinaiya Karthik <Kavinaiya05@gmail.com>
2024-12-17 20:19:19 +0530

    book

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
+// Define the Book schema
 const bookSchema = new mongoose.Schema({
   title: String,
   author: String,
-  // other fields...
+  // Other fields...
 });
 
+// Create and export the Book model
 const Book = mongoose.model('Book', bookSchema);
-
 module.exports = Book;
