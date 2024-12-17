d0e3602a5e385ae2bd4b31a1a6af0641f8a51069
Kavinaiya Karthik <Kavinaiya05@gmail.com>
2024-12-17 16:11:52 +0530

    Add heroku-postbuild script and backend server setup

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,11 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  thumbnail: { type: String },
-  ratings: [Number],
-  reviews: [{ user: String, review: String }],
+  status: { type: String, enum: ['currently reading', 'to read', 'completed'], default: 'to read' },
+  addedAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Book", bookSchema);
+module.exports = mongoose.model('Book', bookSchema);
