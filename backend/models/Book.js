462b9541e167119380243ea6b41e27b2c0dbc2ca
Kavinaiya Karthik <Kavinaiya05@gmail.com>
2024-12-17 18:15:58 +0530

    book.js modification

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  status: { type: String, enum: ['currently reading', 'to read', 'completed'], default: 'to read' },
-  addedAt: { type: Date, default: Date.now }
+  title: String,
+  author: String,
+  publishedYear: Number,
 });
 
 module.exports = mongoose.model('Book', bookSchema);
