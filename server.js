6d5be2adcc7b36fd0004c65ea7878581d9f2704f
Kavinaiya Karthik <Kavinaiya05@gmail.com>
2024-12-22 09:46:34 +0530

    Fix case sensitivity for Book.js

--- server.js
+++ server.js
@@ -21,9 +21,13 @@ app.use(cors());
 mongoose.set('strictQuery', false);
 
 // --- MongoDB Connection ---
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected successfully'))
-  .catch(err => console.error('MongoDB connection error:', err));
+const uri = process.env.MONGO_URI || 'your-default-uri';
+mongoose.connect(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 // --- API Endpoints ---
 
